355e2c00cb4a4d39866ba980e42d2c5818281ebc
Rafael Cassau <rafa.cassau@gmail.com>
2019-02-14 21:07:07 -0200

    replace flyweight design pattern refactoring-guru example for improve readlbility

--- structural/flyweight/refactoring-guru.py
+++ structural/flyweight/refactoring-guru.py
@@ -9,160 +9,107 @@ state between multiple objects. In other words,
 the Flyweight saves RAM by caching the same data
 used by different objects.
 """
-from enum import Enum
-from typing import List
-from random import randint
+import json
+from typing import Dict
 
 
-class Color(Enum):
+class Flyweight():
     """
-    enum for simulate colors
+    The Flyweight stores a common portion of the state (also called intrinsic
+    state) that belongs to multiple real business entities. The Flyweight
+    accepts the rest of the state (extrinsic state, unique for each entity)
+    via its method parameters.
     """
-    BLACK = 'black'
-    GREEN = 'green'
-    ORANGE = 'orange'
+
+    def __init__(self, shared_state: str) -> None:
+        self._shared_state = shared_state
+
+    def operation(self, unique_state: str) -> None:
+        s = json.dumps(self._shared_state)
+        u = json.dumps(unique_state)
+        print(f"Flyweight: Displaying shared ({s}) and unique ({u}) state.", end="")
 
 
-class Graphics:
+class FlyweightFactory():
     """
-    util for simule render shapes on screen
+    The Flyweight Factory creates and manages the Flyweight objects. It
+    ensures that flyweights are shared correctly. When the client requests a
+    flyweight, the factory either returns an existing instance or creates a
+    new one, if it doesn't exist yet.
     """
-    def __init__(self):
-        self._color: Color = None
 
-    def set_color(self, color: Color) -> None:
-        self._color = color
+    _flyweights: Dict[str, Flyweight] = {}
 
-    def fill_rect(self, x: int, y: int, width: int, height: int):
-        print('------------------------------------------------')
-        print('      *      ')
-        print('      *      ')
-        print('      *      ')
-        print(f'x: {x}, y: {y}, width: {width}, height: {height}, color: {self._color} ')
+    def __init__(self, initial_flyweights: Dict) -> None:
+        for state in initial_flyweights:
+            self._flyweights[self.get_key(state)] = Flyweight(state)
 
-    def fill_oval(self, x: int, y: int, width: int, height: int):
-        print('------------------------------------------------')
-        print('     ***     ')
-        print('    *****    ')
-        print('     ***     ')
-        print(f'x: {x}, y: {y}, width: {width}, height: {height}, color: {self._color} ')
+    def get_key(self, state: Dict) -> str:
+        """
+        Returns a Flyweight's string hash for a given state.
+        """
 
+        return "_".join(sorted(state))
 
-class TreeType:
-    """
-    Flyweight object
+    def get_flyweight(self, shared_state: Dict) -> Flyweight:
+        """
+        Returns an existing Flyweight with a given state or creates a new
+        one.
+        """
+        key = self.get_key(shared_state)
 
-    contain the "intrinsic" (immutable)
-    and heavy sharable part of tree object
-    """
-    def __init__(self, name: str, color: Color, other_tree_data: str) -> None:
-        self._name: str = name
-        self._color: Color = color
-        self._other_tree_data: str = other_tree_data
+        if not self._flyweights.get(key):
+            print("FlyweightFactory: Can't find a flyweight, creating new one.")
+            self._flyweights[key] = Flyweight(shared_state)
+        else:
+            print("FlyweightFactory: Reusing existing flyweight.")
 
-    def draw(self, graphics: Graphics, x: int, y: int) -> None:
-        graphics.set_color(Color.BLACK)
-        graphics.fill_rect(x - 1, y, 3, 5)
-        graphics.set_color(self._color)
-        graphics.fill_oval(x - 5, y - 10, 10, 10)
+        return self._flyweights[key]
+
+    def list_flyweights(self) -> None:
+        count = len(self._flyweights)
+        print(f"FlyweightFactory: I have {count} flyweights:")
+        print("\n".join(map(str, self._flyweights.keys())), end="")
 
 
-class Tree:
-    """
-    Contains the "extrinsic" (mutable)
-    state unique for each tree
-    """
-    def __init__(self, x: int, y: int, tree_type: TreeType) -> None:
-        self._x: int = x
-        self._y: int = y
-        self.tree_type: TreeType = tree_type
-
-    def draw(self, graphics: Graphics) -> None:
-        self.tree_type.draw(graphics, self._x, self._y)
-
-
-class TreeFactory:
-    """
-    Factory that create cached and pooling Flyweight TreeType objects
-    """
-    tree_types_cache = {}
-
-    @classmethod
-    def get_tree_type(cls, name: str, color: Color, other_tree_data: str) -> TreeType:
-        tree_type: TreeType = cls.tree_types_cache.get(name)
-        if not tree_type:
-            tree_type = TreeType(name, color, other_tree_data)
-            cls.tree_types_cache[name] = tree_type
-
-        return tree_type
-
-
-class Forest:
-    """
-    Forest with we draw
-    """
-    def __init__(self) -> None:
-        self._trees: List[Tree] = []
-
-    def plant_tree(self, x: int, y: int, name: str, color: Color, other_tree_data: str) -> None:
-        tree_type: TreeType = TreeFactory.get_tree_type(name, color, other_tree_data)
-        tree: Tree = Tree(x, y, tree_type)
-        self._trees.append(tree)
-
-    def paint(self, graphics: Graphics) -> None:
-        for tree in self._trees:
-            tree.draw(graphics)
+def add_car_to_police_database(
+    factory: FlyweightFactory, plates: str, owner: str,
+    brand: str, model: str, color: str
+) -> None:
+    print("\n\nClient: Adding a car to database.")
+    flyweight = factory.get_flyweight([brand, model, color])
+    # The client code either stores or calculates extrinsic state and passes it
+    # to the flyweight's methods.
+    flyweight.operation([plates, owner])
 
 
 class Demo:
-    CANVAS_SIZE = 500
-    TREES_TO_DRAW = 1000000
-    TREE_TYPES = 2
 
     def run(self) -> None:
-        forest: Forest = Forest()
+        """
+        The client code usually creates a bunch of pre-populated flyweights in
+        the initialization stage of the application.
+        """
 
-        for _ in range(1, self.TREES_TO_DRAW // self.TREE_TYPES):
-            forest.plant_tree(
-                randint(0, self.CANVAS_SIZE),
-                randint(0, self.CANVAS_SIZE),
-                'Summer Oak',
-                Color.GREEN,
-                'Oak texture stub',
-            )
-            forest.plant_tree(
-                randint(0, self.CANVAS_SIZE),
-                randint(0, self.CANVAS_SIZE),
-                'Autumn Oak',
-                Color.ORANGE,
-                'Autum Oak texture stub',
-            )
+        factory = FlyweightFactory([
+            ["Chevrolet", "Camaro2018", "pink"],
+            ["Mercedes Benz", "C300", "black"],
+            ["Mercedes Benz", "C500", "red"],
+            ["BMW", "M5", "red"],
+            ["BMW", "X6", "white"],
+        ])
 
-        graphics = Graphics()
-        forest.paint(graphics)
+        factory.list_flyweights()
 
-        print(f'{self.TREES_TO_DRAW} trees drawn')
-        print('----------------')
-        print('Memory usage:')
-        print(f'Tree size (8 bytes) * {self.TREES_TO_DRAW}')
-        print(f'+ TreeTypes size (~30 bytes) * {self.TREE_TYPES}')
-        print('----------------')
-        
-        # TREES_TO_DRAW has 8 bytes each
+        add_car_to_police_database(
+            factory, "CL234IR", "James Doe", "BMW", "M5", "red")
 
-        # TREE_TYPES has 30 bytes each, there will be only 2 shareable flyweight objects
-        # this is done by: self.TREE_TYPES * 30
+        add_car_to_police_database(
+            factory, "CL234IR", "James Doe", "BMW", "X1", "red")
 
-        # the mathematical expression is ((1000000 * 8) + (2 * 30)) / 1024 / 1204
-        new_total: int = (self.TREES_TO_DRAW * 8 + self.TREE_TYPES * 30) / 1024 / 1024
+        print("\n")
 
-        # TREES_TO_DRAW has 38 bytes each, it's equal the SUM of (extrinsic + instrinsic) data
-        # because flyweight does not exist in this context
-
-        # the mathematical expression is (1000000 * 38) / 1024 / 1204
-        old_total: int = (self.TREES_TO_DRAW * 38) / 1024 / 1024
-        
-        print(f'Total: {new_total} MB (instead of {old_total} MB)')
+        factory.list_flyweights()
 
 
 demo: Demo = Demo()
